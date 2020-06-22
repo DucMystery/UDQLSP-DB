1035daf97b2344d901f80e82aff13a6977a3db8b
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-06-22 15:22:58 +0700

    Hoan thanh

---
+++ src/main/java/cg/wbd/model/Customer.java
@@ -0,0 +1,88 @@
+package cg.wbd.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Customer implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Customer(String name, String email, String address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Customer setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Customer setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Customer setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Customer setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    @Override
+    public Customer clone() {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setAddress(address);
+        return customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/cg/wbd/service/CustomerServiceImpl.java
@@ -0,0 +1,94 @@
+package cg.wbd.service;
+
+import cg.wbd.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
+public class CustomerServiceImpl implements CustomerService {
+
+    private static List<Customer> customers;
+    private static long autoIncreaseID =0;
+
+    static {
+        customers= asList(
+                new Customer(autoIncreaseID++, "T", "t@codegym.vn", "Da Nang"),
+                new Customer(autoIncreaseID++, "Nhat", "nhat@codegym.vn", "Quang Tri"),
+                new Customer(autoIncreaseID++, "Trang", "trang@codegym.vn", "Ha Noi"),
+                new Customer(autoIncreaseID++, "Nguyen Binh Son", "son@codegym.vn", "Sai Gon"),
+                new Customer(autoIncreaseID++, "Dang Xuan Hoa", "hoa.dang@codegym.vn", "Da Nang")
+        );
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers);
+    }
+
+    @Override
+    public Customer findOne(Long id) {
+        return customers.stream().filter(c->c.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return customer.getId()==null?persist(customer):merge(customer);
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        return customers.stream().map(this::save).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return customers.stream().anyMatch(c->c.getId().equals(id));
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        return ids.stream().map(this::findOne).collect(Collectors.toList());
+    }
+
+    @Override
+    public long count() {
+        return customers.size();
+    }
+
+    @Override
+    public void delete(Long id) {
+        customers.removeIf(c->c.getId().equals(id));
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        delete(customer.getId());
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+        customers.forEach(this::delete);
+    }
+
+    @Override
+    public void deleteAll() {
+        customers=new ArrayList<>();
+    }
+    private Customer persist(Customer customer) {
+        Customer clone = customer.clone();
+        clone.setId(autoIncreaseID++);
+        customers.add(clone);
+        return clone;
+    }
+
+    private Customer merge(Customer customer) {
+        Customer origin = findOne(customer.getId());
+        origin.setName(customer.getName());
+        origin.setEmail(customer.getEmail());
+        origin.setAddress(customer.getAddress());
+        return origin;
+    }
+}
